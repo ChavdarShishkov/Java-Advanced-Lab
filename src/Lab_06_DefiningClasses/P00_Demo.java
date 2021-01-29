9ad280bc8c800ba29b30e1f5cbf85f46e872bc8e
chocho <finaltitan69@gmail.com>
2021-01-29 18:37:04 +0200

    Commit

--- src/Lab_06_DefiningClasses/P00_Demo.java
+++ src/Lab_06_DefiningClasses/P00_Demo.java
@@ -1,5 +1,6 @@
 package Lab_06_DefiningClasses;
 
+
 public class P00_Demo {
     public static void main(String[] args) {
 
---
+++ src/Lab_06_DefiningClasses/Person.java
@@ -0,0 +1,10 @@
+package Lab_06_DefiningClasses;
+
+public class Person {
+    private static String personName;
+    private static int age;
+    private static String job;
+    private static double salary;
+
+
+}
