aaa3eab55d520ad4b97e23bad9a220d19f1fbd3c
chocho <finaltitan69@gmail.com>
2021-02-01 15:39:38 +0200

    Commit

--- src/Lab_06_DefiningClasses/P03_BankAccount/BankAccount.java
+++ src/Lab_06_DefiningClasses/P03_BankAccount/BankAccount.java
@@ -1,14 +1,29 @@
 package Lab_06_DefiningClasses.P03_BankAccount;
 
 public class BankAccount {
+    private static int accountsCount = 1;
+    private static double interestRate = 0.02;
     private int id;
     private double balance;
-    private final static double DEFAULT_INTEREST_RATE = 0.2;
-    private static double interestRate = DEFAULT_INTEREST_RATE;
-    private static int bankAccountCount = 1;
 
-    public BankAccount(int id, double balance) {
-        this.id = id;
-        this.balance = balance;
+    public BankAccount() {
+        this.id = accountsCount;
+        accountsCount++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public double getInterest(int years) {
+        return this.balance * interestRate * years;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
     }
 }
--- src/Lab_06_DefiningClasses/P03_BankAccount/P03_BankAccount.java
+++ src/Lab_06_DefiningClasses/P03_BankAccount/P03_BankAccount.java
@@ -1,12 +1,53 @@
 package Lab_06_DefiningClasses.P03_BankAccount;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class P03_BankAccount {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var reader = new BufferedReader(new InputStreamReader(System.in));
 
+        Map<Integer, BankAccount> vault = new HashMap<>();
 
+        String input = reader.readLine();
+        while (!input.equals("END")) {
+            String[] tokens = input.split("\\s+");
+
+            String output;
+            switch (tokens[0]) {
+                case "Deposit":
+                    int id = Integer.parseInt(tokens[1]);
+                    int amount = Integer.parseInt(tokens[2]);
+                    vault.get(id).deposit(amount);
+                    output = "Deposited " + amount + " to ID" + id;
+                    break;
+
+                case "SetInterest":
+                    double newInterest = Double.parseDouble(tokens[2]);
+                    BankAccount.setInterestRate(newInterest);
+                    output = null;
+                    break;
+
+                case "GetInterest":
+                    id = Integer.parseInt(tokens[1]);
+                    int years = Integer.parseInt(tokens[2]);
+                    double interest = vault.get(id).getInterest(years);
+                    output = String.format("%.2f", interest);
+                    break;
+
+                default:
+                    BankAccount bankAccount = new BankAccount();
+                    vault.put(bankAccount.getId(), bankAccount);
+                    output = "Account " + bankAccount.getId() + " created";
+                    break;
+            }
+            if (output != null) {
+                System.out.println(output);
+            }
+            input = reader.readLine();
+        }
     }
 }
